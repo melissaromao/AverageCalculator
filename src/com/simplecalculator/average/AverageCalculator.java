a6a30f6a191c1b3bee539f4f0adcf2373566f328
Melissa Romão <86786053+melissaromao@users.noreply.github.com>
2024-09-14 10:06:29 -0300

    Add files via upload

---
+++ src/com/simplecalculator/average/AverageCalculator.java
@@ -0,0 +1,25 @@
+package com.simplecalculator.average;
+
+//importando classe fundamental para leitura de entradas de dados
+import java.util.Scanner;
+
+public class AverageCalculator {
+
+	public static void main(String[] args) {
+		//criando objeto Scanner para ler dados do teclado
+		Scanner sc = new Scanner(System.in);
+		//declarando variáveis
+		double x, y, average;
+		//exibindo mensagens
+		System.out.print("Digite o valor da primeira nota: ");
+		//lê dado armazenado na variável de tipo double
+		x = sc.nextDouble();
+		System.out.print("Digite o valor da segunda nota: ");
+		y = sc.nextDouble();
+		//atribuindo para a varíavel do tipo double (que por padrão tinha o valor 0.0) o cálculo da média com as notas x e y
+		average = ((x + y) / 2);
+		System.out.print("Com base nas suas notas sua média é: " + average);
+		//encerrando objeto para liberar recursos
+		sc.close();
+	}
+}
